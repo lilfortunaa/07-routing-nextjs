f01c4836fa99cd121f0ba18900ce362641aa2f6b
lilfortunaa <bogdanchikzaichik@gmail.com>
2025-08-30 11:19:46 +0200

    Update page.tsx
    import fix

--- app/@modal/(.)notes/[id]/page.tsx
+++ app/@modal/(.)notes/[id]/page.tsx
@@ -4,7 +4,7 @@ import {
   dehydrate,
   HydrationBoundary,
 } from '@tanstack/react-query';
-import NoteDetailsClient from '../../../components/NoteDetails/NoteDetails.client';
+import NoteDetailsClient from '../../../../components/NoteDetails/NoteDetails.client';
 
 interface NotePageProps {
   params: { id: string };
